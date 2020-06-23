c52a4a23b77e3a93a5fd1c5252aa61e088fc1506
Leonardo Lopes <blopesleo@outlook.com>
2020-06-23 11:12:26 -0300

    Overwritting factory_bot values

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   it '#full_name' do
-    account = create(:account)
-    expect(account.full_name).to start_with("Sr. ")
+    account = create(:account, name: "Bilbo Baggins")
+    expect(account.full_name).to eq("Sr. Bilbo Baggins")
   end
 
   it { expect{create(:account)}.to change{ Account.all.size }.by(1) }
